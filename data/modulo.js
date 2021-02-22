45c82736ec3e9a53eb3f3a2fc40b4811ffa68321
crkj <0soundlive@gmail.com>
2021-02-22 00:44:41 -0500

    tabla de datos en la web

--- data/modulo.js
+++ data/modulo.js
@@ -1,21 +1,32 @@
 import { data } from "./data.js";
 let contador = 0;
 const contenedor = document.querySelector('#data');
+contenedor.className = 'data-container';
 for (let i = 0; i < data.length; i++){
     if (contador <= data.length){
 
+        let items0 = document.createElement('spam');
         let items1 = document.createElement('spam');
         let items2 = document.createElement('spam');
         let items3 = document.createElement('spam');
         let items4 = document.createElement('spam');
         let items5 = document.createElement('spam');
-        items1.textContent += `item ${data[i].item} `;
-        items2.textContent += `description ${data[i].matricula} `;
-        items3.textContent += `zona ${data[i].zona} `;
-        items4.textContent += `descripcion ${data[i].descripcion} `;
-        items5.textContent += `Ciudad ${data[i].ciudad} `;
+        let cont = document.createElement('div');
+        cont.className = 'cont';
+        items0.className = 'items0';
+        items1.className = 'items1';
+        items2.className = 'items2';
+        items3.className = 'items3';
+        items4.className = 'items4';
+        items5.className = 'items5';
+        items0.textContent += `# ${i+1} `;
+        items1.textContent += `${data[i].item} `;
+        items2.textContent += `${data[i].matricula} `;
+        items3.textContent += `${data[i].zona} `;
+        items4.textContent += `${data[i].descripcion} `;
+        items5.textContent += `${data[i].ciudad} `;
         
-        contenedor.append(items1,items2,items3,items4,items5);
+        contenedor.appendChild(cont).append(items0,items1,items2,items3,items4,items5);
 
     }
 }
