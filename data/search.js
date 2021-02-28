9d06cd42683d114fd126c2f4ad0abac8942f8c72
crkj <0soundlive@gmail.com>
2021-02-28 12:15:10 -0500

    pagina y busquedas filtradas 90%

--- data/search.js
+++ data/search.js
@@ -1,37 +1,46 @@
+// alert('fn')
 import { data } from './data.js';
 
-let search = document.getElementById('search');
-const ids = document.getElementById('apartamentos');
+// let contador = 0;
+const search = document.getElementById('search');
+search.className = "search_table";
 
+const id_page = document.getElementById('id_page_name');
+id_page.className = 'id_page';
 
-const insertando = (arg) => {
-    let contador = 0;
+let id_page_text = id_page.textContent;
+// console.log(id_page_text)
 
-    for (let i = 0; i < data.length; i++) {
+let conteo=0;
+for (let i = 0; i < data.length; i++){
 
-        if (data[i].clase == arg) {
-            let elementoContenedor = document.createElement('div');
-            let div0 = document.createElement('div');
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');
-            let div3 = document.createElement('div');
-            let div4 = document.createElement('div');
-            elementoContenedor.className = "contenedorSearch";
-            elementoContenedor.textContent = 'Donde esta'
-            div0.className = "parrafo0";
-            div1.className = "parrafo1";
-            div2.className = "parrafo2";
-            div3.className = "parrafo3";
-            div4.className = "parrafo4";
+    if (data[i].clase == id_page_text){
 
-            div0.textContent = `# ${contador + 1}`;
-            div1.textContent = `${data[i].clase}`;
-            div2.textContent = `${data[i].descripcion}`;
-            div3.textContent = `${data[i].item}`;
-            div4.textContent = `${data[i].zona}`;
-            search.appendChild(elementoContenedor);
-            contador++;
-            console.log(elementoContenedor);        }
+        let item_item = document.createElement('div');
+        item_item.className = 'item_item';
+        
+        let item_dataItem = document.createElement('div');
+        item_dataItem.className = 'item_dataItem icon-image';
+        item_dataItem.textContent += `${data[i].item}`
+
+        let item_dataMatricula = document.createElement('div');
+        item_dataMatricula.textContent += `${data[i].matricula.toLowerCase()}`
+
+        let item_dataZona = document.createElement('div');
+        item_dataZona.textContent += `${data[i].zona.toLowerCase()}`
+
+        let item_dataClase = document.createElement('div');
+        item_dataClase.textContent += `${data[i].clase.toLowerCase()}`
+
+        let item_dataDesc = document.createElement('div');
+        item_dataDesc.textContent += `${data[i].descripcion.toLowerCase()}`
+        
+        let item_dataCity = document.createElement('div');
+        item_dataCity.textContent += `${data[i].ciudad.toLowerCase()}`
+
+        search.appendChild(item_item).append(item_dataItem, item_dataMatricula, item_dataZona,item_dataClase, item_dataDesc, item_dataCity);
+
+        // console.log(conteo+1, data[i].item, data[i].zona, data[i].clase, id_page.textContent);
+        conteo++;
     }
-};
-insertando(ids);
+}
