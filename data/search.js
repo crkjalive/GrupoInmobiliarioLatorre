de0ff1fe0d62ce12bf7d2fb081c0dcfb119e3a95
mauricio_latorre <mauricio.latorre@gmail.com>
2021-02-27 19:09:41 -0500

    search no imprime

--- data/search.js
+++ data/search.js
@@ -1,53 +1,37 @@
 import { data } from './data.js';
 
-let contador = 0;
-const search = document.getElementById('search');
-const combo = document.getElementById('combo');
+let search = document.getElementById('search');
+const ids = document.getElementById('apartamentos');
 
-let itemSearch;
 
-function miFuncion () {
-    itemSearch = combo.value;
-    if (itemSearch == "Casa") {
-        const pin1 = "Casa";
-        insertando(pin1)
-    }
-    else if (itemSearch == "Bodega") {
-        const pin2 = "Bodega";
-        insertando(pin2)
-    }
-    else if (itemSearch == "Garaje") {
-        const pin3 = "Garaje";
-        insertando(pin3)
-    }
-};
+const insertando = (arg) => {
+    let contador = 0;
 
-function insertando (arg) {
     for (let i = 0; i < data.length; i++) {
-        if (contador <= data.length) {
-            if (data[i].clase == itemSearch) {
-                let elementoContenedor = document.createElement('div');
-                let elementoP0 = document.createElement('div');
-                let elementoP1 = document.createElement('div');
-                let elementoP2 = document.createElement('div');
-                let elementoP3 = document.createElement('div');
-                let elementoP4 = document.createElement('div');
-                elementoContenedor.className = "contenedorSearch";
-                elementoP0.className = "parrafo0";
-                elementoP1.className = "parrafo1";
-                elementoP2.className = "parrafo2";
-                elementoP3.className = "parrafo3";
-                elementoP4.className = "parrafo4";
 
-                elementoP0.textContent += `# ${contador + 1}`;
-                elementoP1.textContent += `${data[i].clase}`;
-                elementoP2.textContent += `${data[i].descripcion}`;
-                elementoP3.textContent += `${data[i].item}`;
-                elementoP4.textContent += `${data[i].zona}`;
-                search.append(elementoP0, elementoP3, elementoP4, elementoP2, elementoP1);
-                console.log(data[i].clase, data[i].descripcion);
-                contador++
-            }
-        }
+        if (data[i].clase == arg) {
+            let elementoContenedor = document.createElement('div');
+            let div0 = document.createElement('div');
+            let div1 = document.createElement('div');
+            let div2 = document.createElement('div');
+            let div3 = document.createElement('div');
+            let div4 = document.createElement('div');
+            elementoContenedor.className = "contenedorSearch";
+            elementoContenedor.textContent = 'Donde esta'
+            div0.className = "parrafo0";
+            div1.className = "parrafo1";
+            div2.className = "parrafo2";
+            div3.className = "parrafo3";
+            div4.className = "parrafo4";
+
+            div0.textContent = `# ${contador + 1}`;
+            div1.textContent = `${data[i].clase}`;
+            div2.textContent = `${data[i].descripcion}`;
+            div3.textContent = `${data[i].item}`;
+            div4.textContent = `${data[i].zona}`;
+            search.appendChild(elementoContenedor);
+            contador++;
+            console.log(elementoContenedor);        }
     }
 };
+insertando(ids);
