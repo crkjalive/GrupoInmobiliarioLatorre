b30abbfb47d63d339b3032d27edc3d34a20bf4e3
crkj <0soundlive@gmail.com>
2021-03-01 03:33:57 -0500

    post-it

--- data/search.js
+++ data/search.js
@@ -1,46 +1,82 @@
 // alert('fn')
+
 import { data } from './data.js';
 
-// let contador = 0;
-const search = document.getElementById('search');
-search.className = "search_table";
+window.load = () => {
 
-const id_page = document.getElementById('id_page_name');
-id_page.className = 'id_page';
+    const busqueda = () => {
 
-let id_page_text = id_page.textContent;
-// console.log(id_page_text)
+        const search = document.getElementById('search');
+        search.className = "search_table";
 
-let conteo=0;
-for (let i = 0; i < data.length; i++){
+        const id_page = document.querySelector('body');
+        // id_page.className = 'id_page';
 
-    if (data[i].clase == id_page_text){
+        let id_page_text = id_page.title;
+        // console.log(id_page_text)
 
-        let item_item = document.createElement('div');
-        item_item.className = 'item_item';
-        
-        let item_dataItem = document.createElement('div');
-        item_dataItem.className = 'item_dataItem icon-image';
-        item_dataItem.textContent += `${data[i].item}`
+        let conteo = 0;
+        for (let i = 0; i < data.length; i++) {
 
-        let item_dataMatricula = document.createElement('div');
-        item_dataMatricula.textContent += `${data[i].matricula.toLowerCase()}`
+            if (data[i].clase == id_page_text) {
 
-        let item_dataZona = document.createElement('div');
-        item_dataZona.textContent += `${data[i].zona.toLowerCase()}`
+                let item_item = document.createElement('div');
+                item_item.className = 'item_item';
 
-        let item_dataClase = document.createElement('div');
-        item_dataClase.textContent += `${data[i].clase.toLowerCase()}`
+                let item_conteo = document.createElement('div');
+                item_conteo.className = 'item_conteo';
+                item_conteo.textContent += `Post-it: ${conteo + 1}`
 
-        let item_dataDesc = document.createElement('div');
-        item_dataDesc.textContent += `${data[i].descripcion.toLowerCase()}`
-        
-        let item_dataCity = document.createElement('div');
-        item_dataCity.textContent += `${data[i].ciudad.toLowerCase()}`
+                let item_dataLink = document.createElement('a');
+                if (data[i].link) {
+                    item_dataLink.className = 'item_dataLink icon-mobile';
+                    item_dataLink.href += `${data[i].link}`;
+                    item_dataLink.textContent += ` Google Maps`;
+                    item_dataLink.target += '_blank';
+                    item_dataLink.style.color = "#FD971F";
+                }
 
-        search.appendChild(item_item).append(item_dataItem, item_dataMatricula, item_dataZona,item_dataClase, item_dataDesc, item_dataCity);
+                let item_dataItem = document.createElement('div');
+                item_dataItem.className = 'item_dataItem';
+                item_dataItem.textContent += `ID: ${data[i].item}`
 
-        // console.log(conteo+1, data[i].item, data[i].zona, data[i].clase, id_page.textContent);
-        conteo++;
+                let item_dataMatricula = document.createElement('div');
+                item_dataMatricula.className = "item_dataMatricula";
+                item_dataMatricula.textContent += `Matricula: ${data[i].matricula.toLowerCase()}`
+
+                let item_dataZona = document.createElement('div');
+                item_dataZona.className = "item_dataZona";
+                item_dataZona.textContent += `Zona: ${data[i].zona.toLowerCase()}`
+
+                let item_dataClase = document.createElement('div');
+                item_dataClase.className = "item_dataClase";
+                item_dataClase.textContent += `Tipo: ${data[i].clase.toLowerCase()}`
+
+                let item_dataDesc = document.createElement('div');
+                item_dataDesc.className = "item_dataDesc";
+                item_dataDesc.textContent += `Ubicación: ${data[i].descripcion.toLowerCase()}`
+
+                let item_dataCity = document.createElement('div');
+                item_dataCity.className = "item_dataCity";
+                item_dataCity.textContent += `Ciudad: ${data[i].ciudad.toLowerCase()}`
+
+                search.appendChild(
+                    item_item)
+                    .append(
+                        item_dataLink,
+                        item_conteo,
+                        item_dataItem,
+                        item_dataZona,
+                        item_dataClase,
+                        item_dataDesc,
+                        item_dataCity,
+                    );
+
+                // console.log(conteo+1, data[i].item, data[i].zona, data[i].clase, id_page.textContent);
+                conteo++;
+            }
+        }
     }
+    busqueda();
 }
+window.load();
--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,15 @@
-const data_menu1 = ["apartamentos",'casas','bodegas'];
+const data_menu1 = ["apartamentos", 'casas', 'bodegas'];
 const data_menu2 = ['garajes', 'depositos', 'locales'];
-const data_redes = ['whatsapp','facebook','instagram'];
+const data_redes = ['whatsapp', 'facebook', 'instagram', 'Google'];
 
 let ul_container1 = document.getElementById('ul_container1');
 let ul_container2 = document.getElementById('ul_container2');
 
 const numero = "3192205259";
 
-const menu_create = (contenedor,array) => {
+const menu_create = (contenedor, array) => {
     let data_item;
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let a = document.createElement('a');
         a.className = 'a_li';
         a.href = `${array[i].toLowerCase()}.html`;
@@ -20,8 +20,8 @@ const menu_create = (contenedor,array) => {
         contenedor.appendChild(a).appendChild(li);
     }
 }
-menu_create(ul_container1,data_menu1);
-menu_create(ul_container2,data_menu2);
+menu_create(ul_container1, data_menu1);
+menu_create(ul_container2, data_menu2);
 
 const logo_create = () => {
     let a = document.createElement('a');
@@ -37,22 +37,35 @@ logo_create();
 
 const sociales_create = () => {
     const redes_sociales = document.getElementById('redes_sociales');
-    const a = document.createElement('a');
-    const b = document.createElement('a');
-    const c = document.createElement('a');
-    a.target = '_blank';
-    b.target = '_blank';
-    c.target = '_blank';
-    let whatsapp = a;
-    let facebook = b;
-    let instagram = c;
-    whatsapp.className = " link_icon icon-whatsapp"
-    facebook.className = " link_icon icon-facebook"
-    instagram.className = " link_icon icon-instagram"
+
+    const whatsapp = document.createElement('a');
+    whatsapp.className = " link_icon icon-whatsapp";
+    whatsapp.target = '_blank';
     whatsapp.href = `https://api.whatsapp.com/send?phone=57${numero}/`;
-    facebook.href = `https://wwwgoogle.com/`;
-    instagram.href = `https://youtube.com/`;
-    redes_sociales.append(whatsapp, facebook, instagram);
+
+    const facebook = document.createElement('a');
+    facebook.className = " link_icon icon-facebook";
+    facebook.target = '_blank';
+    facebook.href = `https://www.facebook.com/grupoinmobiliario.latorre.7/`;
+
+    const youtube = document.createElement('a');
+    youtube.className = " link_icon icon-youtube";
+    youtube.target = '_blank';
+    youtube.href = `https://youtube.com/`;
+
+    const googleMaps = document.createElement('a');
+    googleMaps.className = " link_icon icon-mobile"
+    googleMaps.target = '_blank';
+    googleMaps.href = `https://www.google.com/maps/`;
+    googleMaps.alt = `google.com maps`;
+
+    redes_sociales
+        .append(
+            googleMaps,
+            whatsapp,
+            facebook,
+            youtube,
+        );
 }
 sociales_create();
 
