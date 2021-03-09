79cda743aa4a3f453b58ae195b59e9ee9d9439cb
crkj <0soundlive@gmail.com>
2021-03-09 15:42:47 -0500

    imagenes optimizadas y articulos main

--- js/main.js
+++ js/main.js
@@ -2,50 +2,65 @@ const items = [
 	{	
 		id:0,
 		image:"./venta/casa1.jpg",
-		title:"<strong>Venta Casa</strong> <br> Barrio: San Jorge 162mts²",
-		description:"Casa de 6mts x 27mts <br> <strong>Primer Piso:</strong> 3 alcobas Sala Comedor Cocina, Baño, Patio y Garaje",
-		add: "<strong>Segundo piso:</strong> 4 alcobas, sala comedor, cocina, baño y terraza cubierta.",
-		price: "<b>Negociables</b> $ 420.000.000"
+		title:"<strong>Venta Casa</strong> <br> Barrio: San Jorge ",
+		description:"Casa 162mts² <br> <strong>Primer Piso:</strong> 3 alcobas Sala Comedor Cocina, Baño, Patio y Garaje",
+		add: "<strong>Segundo piso:</strong> 4 habitaciones, sala comedor, cocina, baño <br> terraza cubierta.",
+		link: "https://www.facebook.com/photo?fbid=124990429629071&set=pcb.124990152962432",
+		price: "<b>Negociables</b> $420.000.000"
 	},
 	{	
 		id:1,
-		image: "./img/imagenDemo.jpg",
-		title:"Apartamento Modelo",
-		description:"3 alcoba 2 baños sala comedor cocina y cuarto de ropas",
-		add: "parqueadero cubierto",
-		price: "<b>Negociables</b> $ 80.000.000"
+		image: "./venta/lote1.jpg",
+		title:"<strong>Venta Lote</strong> <br> Sector de Suba, Bogotá",
+		description:"Se vende lote en Suba <br> Area de 20.000 metros <br> Estrato 3 <br> para hacer vivienda",
+		add: "Venta Directa",
+		link: "https://www.facebook.com/photo?fbid=124989882962459&set=pcb.124989949629119",
+		price: "<b>Negociables</b> $9'500.000.000"
 	},
 	{	
 		id:2,
-		image: "./img/imagenDemo.jpg",
-		title:"Apartamento Modelo",
-		description:"3 alcoba 2 baños sala comedor cocina y cuarto de ropas",
-		add: "parqueadero cubierto",
-		price: "<b>Negociables</b> $ 80.000.000"
+		image: "./venta/bodega1.jpg",
+		title:"<strong>Venta Bodega</strong> <br> Sector de Alamos, Bogotá",
+		description:"Bodega Aeropuerto Alamos <br> Area de Terreno 2.200 metros",
+		add: "",
+		link: "https://www.facebook.com/photo/?fbid=124979689630145&set=pcb.124979826296798",
+		price: "<b>Negociables</b> $2'200.000.000"
 	},
 	{	
 		id:3,
-		image: "./img/imagenDemo.jpg",
-		title:"Apartamento Modelo",
-		description:"3 alcoba 2 baños sala comedor cocina y cuarto de ropas",
-		add: "parqueadero cubierto",
-		price: "<b>Negociables</b> $ 80.000.000"
+		image: "./venta/bodegaFontibon.jpg",
+		title:"<strong>Venta Bodega</strong> <br> Sector de Fontibon, Bogotá",
+		description:"Fontibon Zona Industrial, mt² a 2 millones",
+		add: "28.000 mts² de lote <br> 16.000 mts² de Construcción",
+		link: "https://www.facebook.com/photo?fbid=124994629628651&set=pcb.124996576295123",
+		price: "<b>Negociables</b> $28.000.000.000"
 	},
 	{	
-		id:2,
-		image: "./img/imagenDemo.jpg",
-		title:"Apartamento Modelo",
-		description:"3 alcoba 2 baños sala comedor cocina y cuarto de ropas",
-		add: "parqueadero cubierto",
-		price: "<b>Negociables</b> $ 80.000.000"
+		id:4,
+		image: "./venta/res1.jpg",
+		title:"<strong>Venta Edificio</strong> <br> Sector del Restrepo, Bogotá",
+		description:"Bodega de 3 pisos, <br> uso comercial, con local",
+		add: "464 mts²",
+		link: "https://www.facebook.com/photo?fbid=125006099627504&set=pcb.125006222960825",
+		price: "<b>Negociables</b> $1'500.000.000"
 	},
 	{	
-		id:3,
-		image: "./img/imagenDemo.jpg",
-		title:"Apartamento Modelo",
+		id:5,
+		image: "./venta/parqueadero1.jpg",
+		title:"<strong>Venta Parqueadero</strong> <br> Sector del Restrepo, Bogotá",
+		description:"8 mts de frente por 28 mts de fondo <br> ",
+		add: "Parqueadero rentable 224 mts²",
+		link: "https://www.facebook.com/photo?fbid=125010012960446&set=a.124979822963465",
+		price: "<b>Negociables</b> $1'000.000.000"
+	},
+	{	
+		id:6,
+		image: "./venta/spa1.jpg",
+		title:"<strong>Venta Spa</strong> <br> Sector del Castilla, Bogotá",
 		description:"3 alcoba 2 baños sala comedor cocina y cuarto de ropas",
 		add: "parqueadero cubierto",
-		price: "<b>Negociables</b> $ 80.000.000"
+		link: "https://www.facebook.com/photo?fbid=125011449626969&set=pcb.125011846293596",
+		price: "<b>Negociables</b> $80.000.000"
 	},
 ];
 
@@ -56,11 +71,12 @@ let section = document.getElementById("section_articulos")
 const indexardo = () => {
 
 class Batman {
-	constructor(title, image, description,add,price) {
+	constructor(title, image, description,add,link,price) {
 		this.title = `${title}`;
 		this.image = `${image}`;
 		this.description = `${description}`;
 		this.add = `${add}`;
+		this.link = `${link}`;
 		this.price = `${price}`;
 	}
 }
@@ -74,6 +90,7 @@ const send = () => {
 			`${dataBase[counter].image}`,
 			`${dataBase[counter].description}`,
 			`${dataBase[counter].add}`,
+			`${dataBase[counter].link}`,
 			`${dataBase[counter].price}`,)
 
 		section.innerHTML += `
@@ -87,6 +104,7 @@ const send = () => {
 					<h3 class="item_title">${robin.title}</h3>
 					<p class="item_description">${robin.description}</p>
 					<p class="item_add">${robin.add}</p>
+					<a href="${robin.link}" class="item_link icon-facebook" target="_blank"> Facebook Fotos</a>
 					<p class="item_price">${robin.price}</p>
 					<div class="dibujo"></div>
 					<div class="dibujo2"></div>
